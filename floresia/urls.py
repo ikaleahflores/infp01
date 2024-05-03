f84e62f84825f4dd37c751f84f3239bf306fb08a
ikaleahflores <ikaleahaflores@gmail.com>
2024-05-03 17:31:17 +0800

    register done

--- floresia/urls.py
+++ floresia/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [ 
     path('', views.home, name='home'), 
+    path('register', views.register_user, name='register'),
     #path('login/', views.login_user, name='login'), 
 ] 
--- floresia/views.py
+++ floresia/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
+from django.contrib import messages 
 # Create your views here.
 
 
 def home(request):
     #do something
     return render(request, 'home.html')    
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            #authenticate
+            username = request.POST('username')
+            password = request.POST('password')
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+
+            return render('home.html')
+    else:
+        form = SignUpForm()
+        return render(request,'register.html', {'form':form})
+
+
+    return render(request, 'register.html', {'form':form})
+    
