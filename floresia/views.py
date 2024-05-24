f5025e97b061aa62b9023120de1cac921ba95399
ikaleahflores <ikaleahaflores@gmail.com>
2024-05-24 17:12:20 +0800

    records done

--- floresia/views.py
+++ floresia/views.py
@@ -3,12 +3,28 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from .forms import SignUpForm
 from django.contrib import messages 
+from .models import *
 # Create your views here.
 
 
 def home(request):
-    #do something
-    return render(request, 'home.html')    
+    records = Record.objects.all()
+    # Check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {'records':records})
+ 
 
 def register_user(request):
     if request.method == 'POST':
